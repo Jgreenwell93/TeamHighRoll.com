4ecb0cdb242f8bd7eb733cfa42226bd6f6a2bdcc
Joseph Greenwell <joeygreenwell@ymail.com>
2022-03-09 17:50:50 -0600

    first cleanup of functionality, removing the need for ID's and button elements for a better function. old elements removed

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -10,28 +10,19 @@ const nickModal = document.getElementById("nickModal");
 const royModal = document.getElementById("royModal");
 const treyModal = document.getElementById("treyModal");
 
-// Get the button that opens the modal
-const alecBtn = document.getElementById("alecBtn");
-const alexBtn = document.getElementById("alexBtn");
-const aubreyBtn = document.getElementById("aubreyBtn");
-const charlieBtn = document.getElementById("charlieBtn");
-const garretBtn = document.getElementById("garretBtn");
-const josephBtn = document.getElementById("josephBtn");
-const michaelBtn = document.getElementById("michaelBtn");
-const nickBtn = document.getElementById("nickBtn");
-const royBtn = document.getElementById("royBtn");
-const treyBtn = document.getElementById("treyBtn");
+// Get the pseudo button that opens the modal
+const openBtn = document.getElementsByClassName("card-link-photo");
 
 // gets all modals to globally set the display to none on close
 const modals = document.getElementsByClassName("modal");
 
 // Get the <span> element that closes the modal
-const close = document.getElementsByClassName("close");
+const closeBtn = document.getElementsByClassName("close");
 
 // When the user clicks on <span> (x), close the modal
 // for each element with the class "close" add an onclick event listener that performs function
-for (let i = 0; i < close.length; i++) {
-  close[i].addEventListener("click", function () {
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].addEventListener("click", function () {
     // for each element with the class "modal" when onclick event is triggered set display to none
     for (let i = 0; i < modals.length; i++) {
       modals[i].style.display = "none";
@@ -39,36 +30,17 @@ for (let i = 0; i < close.length; i++) {
   });
 }
 
-// When the user clicks on the button, open the modal
-alecBtn.onclick = function () {
-  alecModal.style.display = "block";
-}
-alexBtn.onclick = function () {
-  alexModal.style.display = "block";
-}
-aubreyBtn.onclick = function () {
-  aubreyModal.style.display = "block";
-}
-charlieBtn.onclick = function () {
-  charlieModal.style.display = "block";
-}
-garretBtn.onclick = function () {
-  garretModal.style.display = "block";
-}
-josephBtn.onclick = function () {
-  josephModal.style.display = "block";
-}
-michaelBtn.onclick = function () {
-  michaelModal.style.display = "block";
-}
-nickBtn.onclick = function () {
-  nickModal.style.display = "block";
-}
-royBtn.onclick = function () {
-  royModal.style.display = "block";
-}
-treyBtn.onclick = function () {
-  treyModal.style.display = "block";
+
+// When the user clicks on the player photo/ pseudo button, open the corresponding modal. modal elements must be in the same order as card elemtns
+for (let m = 0; m < openBtn.length; m++) {
+  openBtn[m].addEventListener("click", function (event) {
+    // for each element with the class "modal" when onclick event is triggered set display to none
+    for (let m = 0; m < modals.length; m++) {
+      if (event.target == openBtn[m]){
+        modals[m].style.display = "block";
+      }
+  }
+  });
 }
 
 // When the user clicks anywhere outside of the modal, close it
